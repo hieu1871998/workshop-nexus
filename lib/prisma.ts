e59831a5381942f72d5ac6d69e3fb889f333dcba
Vi Hieu <hieu.vi@bluebottle.digital>
2023-08-27 16:54:12 +0700

    feat: use prisma edge with accelerate
    Signed-off-by: Vi Hieu <hieu.vi@bluebottle.digital>

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,13 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
 declare global {
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined
 }
 
-const prisma = global.prisma || new PrismaClient()
+const prisma = new PrismaClient().$extends(withAccelerate()) as unknown as PrismaClient
 
-if (process.env.NODE_ENV === 'development') global.prisma = prisma
+if (process.env.NODE_ENV === 'development') global.prisma = prisma as unknown as PrismaClient
 
 export default prisma
