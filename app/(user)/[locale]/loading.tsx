784165594118a99cea89f651c0a41a788b1f0404
Vi Hieu <hieu.vi@bluebottle.digital>
2023-08-28 17:35:17 +0700

    chore: tweak loading page
    Signed-off-by: Vi Hieu <hieu.vi@bluebottle.digital>

--- app/(user)/[locale]/loading.tsx
+++ app/(user)/[locale]/loading.tsx
@@ -2,8 +2,11 @@ import { PulsingLoader } from '@components/PulsingLoader'
 
 const LoadingPage = () => {
   return (
-    <main className='-mt-16 min-h-screen container mx-auto flex justify-center items-center'>
+    <main className='-mt-16 min-h-screen container mx-auto flex flex-col justify-center items-center'>
       <PulsingLoader />
+      <p className='text-base font-medium'>
+        Loading...
+      </p>
     </main>
   )
 }
