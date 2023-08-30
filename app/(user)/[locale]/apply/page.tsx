6c97f10894e6b2d2315652a1102b5ead712a1de9
Vi Hieu <hieu.vi@bluebottle.digital>
2023-08-30 21:49:57 +0700

    style: tweak apply page
    Signed-off-by: Vi Hieu <hieu.vi@bluebottle.digital>

--- app/(user)/[locale]/apply/page.tsx
+++ app/(user)/[locale]/apply/page.tsx
@@ -7,7 +7,7 @@ const BookingPage = async () => {
   const session = await getServerSession(authOptions)
 
   return (
-    <main className='container mx-auto px-4 min-h-screen max-w-5xl -mt-16'>
+    <main className='container mx-auto px-4 max-w-5xl'>
       <div className='grid grid-cols-1 sm:grid-cols-2 h-full gap-x-10'>
         <ApplyPageTitleSection />
         <section className='col-span-1 h-full flex flex-col justify-center'>
